4688374689921c57de7fc1f1a094d9c76861597f
naveen <naveenbaloni018@gmail.com>
2025-02-15 01:33:04 +0530

    second commit

--- frontend/src/components/common/BudgetCard.jsx
+++ frontend/src/components/common/BudgetCard.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const BudgetCard = () => {
-  return (
-    <div className='w-80 h-40 rounded-lg bg-gray-200'>
-     
-    </div>
-  )
+
+    return (
+        <Link className='w-80 h-40 rounded-lg bg-gray-200 hover:shadow-xl '>
+            <div className='py-3 px-3 h-full'>
+                <div className='flex justify-between items-center py-4'>
+                    <div className='size-16 rounded-full bg-red-400'>
+                        <img src="" alt="" />
+                    </div>
+                    <h1>Price</h1>
+                </div>
+                <div className='border'>asdf</div>
+            </div>
+        </Link>
+    )
 }
 
 export default BudgetCard
--- frontend/src/components/layouts/Budget.jsx
+++ frontend/src/components/layouts/Budget.jsx
@@ -5,12 +5,14 @@ import BudgetCard from '../common/BudgetCard';
 
 const Budget = () => {
   return (
-    <div className='flex-1 bg-white h-full px-2 py-3'>
-      <div className='flex flex-wrap gap-3 max-h-full overflow-y-auto'>
-      <button className='w-80 cursor-pointer h-40 bg-sky-200 backdrop-blur-md rounded-lg flex items-center justify-center'>
-        <GrAdd size={40} />
-      </button>
-      <BudgetCard />
+    <div className='flex-1 bg-gray-100 h-full px-2 py-3'>
+      <div className='flex flex-wrap rounded-lg gap-3 max-h-full py-5 bg-white px-3 overflow-y-auto'>
+        <button className='w-80 hover:shadow-xl cursor-pointer h-40 bg-sky-200 backdrop-blur-md rounded-lg flex items-center justify-center'>
+          <GrAdd size={40} />
+        </button>
+        <BudgetCard />
+        <BudgetCard />
+        <BudgetCard />
       </div>
     </div>
   )
